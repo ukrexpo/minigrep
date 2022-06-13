f050c242cb27d4b050047ca726893b6e7ac848bc
Vitalii Lishcheniuk <ukrexpo@gmail.com>
2022-06-13 19:34:34 +0300

    Added struct Config

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ use std::env;
 use std::fs;
 
 fn main() {
-    fn parse_config(args: &[String]) -> (&str, &str) {
-        let query = &args[1];
-        let filename = &args[2];
-        (query, filename)
+    struct Config {
+        query: String,
+        filename: String,
+    }
+    
+    fn parse_config(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config {query, filename}
     }
 
     let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
-
-    let (query, filename) = parse_config(&args);
     
-    println!("Query: {}", query);
-    println!("Filename: {}", filename);
 
-    let content = fs::read_to_string(filename).expect("Something goes wrong");
+    let config = parse_config(&args);
+    
+    println!("Query: {}", config.query);
+    println!("Filename: {}", config.filename);
+
+    let content = fs::read_to_string(config.filename).expect("Something goes wrong");
     println!("Content: {}", content);
 }
