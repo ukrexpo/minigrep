b58717624c4b7353baf995b466301cff9e76ee82
Vitalii Lishcheniuk <ukrexpo@gmail.com>
2022-08-11 22:37:40 +0200

    Added run(config)

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,23 @@ use std::env;
 use std::fs;
 use std::process;
 
-fn main() {
-    struct Config {
-        query: String,
-        filename: String,
-    }
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    impl Config {
-        fn new(args: &[String]) -> Result<Config, &'static str> {
-            if args.len() < 3 {
-                panic!("Expected 2 arguments");
-            }
-            let query = args[1].clone();
-            let filename = args[2].clone();
-            Ok(Config {query, filename})
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            panic!("Expected 2 arguments");
         }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config {query, filename})
     }
+}
 
+fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Parsing arguments error: {}", err);
@@ -30,6 +30,10 @@ fn main() {
     println!("Query: {}", config.query);
     println!("Filename: {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let content = fs::read_to_string(config.filename).expect("Something goes wrong");
     println!("Content: {}", content);
 }
